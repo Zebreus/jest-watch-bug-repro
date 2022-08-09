5408b2be1bdc31bdba4b8d81ea5bba3337d92c90
Zebreus <lennarteichhorn@googlemail.com>
2022-08-09 13:59:41 +0200

    Add second file

--- main.test.js
+++ main.test.js
@@ -1,5 +1,11 @@
-const thing = require("./thing")
+const alpha = require("./alpha")
+const beta = require("./beta")
 
-test("coverage test", () => {
-  expect(thing(5, "-")).toBe(-5)
+test("coverage test alpha", () => {
+  expect(alpha(5, "-")).toBe(-5)
 })
+
+test("coverage test beta", () => {
+  expect(beta(5, "-")).toBe(-5)
+})
+
